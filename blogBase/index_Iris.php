766ce07dbebcfe6f8a01b50e418f549531677e4d
wangly049 <wangly049@163.com>
2023-04-24 15:36:51 -0700

    add pages

--- blogBase/index_Iris.php
+++ blogBase/index_Iris.php
@@ -20,6 +20,8 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Other/html.html to edit this temp
     </head>
 
     <body>
+        
+        this is the test part.
         <div class ="container">
             <div class = "row" style = "margin: 0px; background: #efefef">
                 <div class ="col-md-2"></div> 
